c17f51228fa09f5a0ad85f2337dd124077c8701f
Philipp Enz <programming@phenz.de>
2022-06-23 16:41:33 +0200

    delete function and add deleteButton

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -4,6 +4,11 @@ import db from '../db.js';
 
 const useStore = create(set => ({
 	db: [...db],
+	deleteBankaccount: id => {
+		set(state => {
+			return {db: state.db.filter(bankAccount => bankAccount.id !== id)};
+		});
+	},
 }));
 
 export default useStore;
