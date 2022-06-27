554c8557ddd590d52210ed40710634fe2b82a257
Philipp Enz <programming@phenz.de>
2022-06-27 15:16:16 +0200

    check  stylelint and eslint on git

--- src/components/Navigation/styled.js
+++ src/components/Navigation/styled.js
@@ -5,19 +5,20 @@ export const Ul = styled.ul`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-around;
-	padding: 1em;
-	margin-bottom: 2em;
 	top: 0;
+	right: 5%;
+	margin-bottom: 2em;
+	padding: 1em;
 	border: 3px solid var(--blue);
 	border-radius: 20px;
 	box-shadow: 5px 5px 5px var(--darkgray);
-	right: 5%;
 `;
 
 export const StyledNavLink = styled(NavLink)`
 	list-style: none;
-	text-decoration: none;
 	color: var(--textcolor);
+	text-decoration: none;
+
 	&:hover {
 		transform: scale(1.2);
 		color: var(--gradient2);
@@ -29,8 +30,8 @@ export const StyledNavLink = styled(NavLink)`
 `;
 
 export const Icon = styled.svg`
-	height: 2em;
-	width: 2em;
 	position: absolute;
+	width: 2em;
+	height: 2em;
 	right: 5%;
 `;
