08e727036f223c889ac8bfa59f0a0370fe33f95f
dananhsl <dananhsl@users.noreply.github.com>
2022-06-29 10:39:55 +0000

    deploy: d18a4e9d052f76223be49bf4096564a36949e8c2

---
+++ main.838f06af.iframe.bundle.js
@@ -0,0 +1,58 @@
+(globalThis.webpackChunkreact_app=globalThis.webpackChunkreact_app||[]).push([[179],{"./src/components/AddNewAccount/AddNewAccount.stories.js":()=>{},"./src/components/BankAccount/BankAccount.stories.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>BankAccount_stories});const Section=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js").ZP.section`
+	margin: 20px 100px;
+	padding: 10px;
+	border: 4px solid var(--blue);
+	border-radius: 30px;
+
+	@media only screen and (max-width: 600px) {
+		margin: 20px 20px;
+	}
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function BankAccount(props){return(0,jsx_runtime.jsx)(jsx_runtime.Fragment,{children:(0,jsx_runtime.jsxs)(Section,{children:[(0,jsx_runtime.jsx)("h2",{children:props.accountName}),(0,jsx_runtime.jsx)("h3",{children:props.bankName}),(0,jsx_runtime.jsxs)("p",{children:[props.accountValue," €"]}),(0,jsx_runtime.jsx)("button",{type:"button",onClick:props.onNavigate,children:"Transactions"}),(0,jsx_runtime.jsx)("button",{type:"button",onClick:props.onDelete,children:"Delete"})]})})}const Default=(args=>(0,jsx_runtime.jsx)(BankAccount,{...args})).bind({});Default.args={accountName:"Girokonto",bankName:"Volksbank",accountValue:200,onDelete:()=>{console.log("delete")}};const BankAccount_stories={title:"Bankaccount",component:BankAccount};Default.parameters={storySource:{source:"args => <BankAccount {...args} />"},...Default.parameters};const __namedExportsOrder=["Default"]},"./src/components/Button/Button.stories.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>Button_stories});var react_router=__webpack_require__("./node_modules/react-router/index.js");function rem(n){return`${n}rem`}const styled=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js").ZP.button`
+	padding: 0.5em 1em;
+	border: 1px solid rgba(0, 0, 0, 0.3);
+	border-radius: ${function pxToRem(pixel){let rootFontSize=arguments.length>1&&void 0!==arguments[1]?arguments[1]:16;return rem(pixel/rootFontSize)}(4)};
+	background: #fae;
+	color: #000;
+	font-size: 1em;
+
+	&:hover {
+		background: #d8b;
+	}
+
+	&:active {
+		background: #bad;
+	}
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function Button(){const navigate=(0,react_router.s0)();return(0,jsx_runtime.jsx)(styled,{onClick:()=>navigate("/"),children:"Return to Dashboard"})}const Default=function Default(){return(0,jsx_runtime.jsx)(Button,{children:"Button"})},Button_stories={title:"Button",component:Button};Default.parameters={storySource:{source:"function Default() {\n\treturn <Button>Button</Button>;\n}"},...Default.parameters};const __namedExportsOrder=["Default"]},"./src/components/InputLabel/InputLabel.stories.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>InputLabel_stories});var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function InputLabel(_ref){let{children}=_ref;return(0,jsx_runtime.jsx)("label",{children})}const Default=function Default(){return(0,jsx_runtime.jsx)(InputLabel,{children:"Input Label"})},InputLabel_stories={title:"InputLabel",component:InputLabel};Default.parameters={storySource:{source:"function Default() {\n\treturn <InputLabel>Input Label</InputLabel>;\n}"},...Default.parameters};const __namedExportsOrder=["Default"]},"./.storybook/preview.js-generated-config-entry.js":(__unused_webpack_module,__unused_webpack___webpack_exports__,__webpack_require__)=>{"use strict";var preview_namespaceObject={};__webpack_require__.r(preview_namespaceObject),__webpack_require__.d(preview_namespaceObject,{__namedExportsOrder:()=>__namedExportsOrder,decorators:()=>decorators,parameters:()=>parameters});var ClientApi=__webpack_require__("./node_modules/@storybook/client-api/dist/esm/ClientApi.js");const GlobalStyle=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js").vJ`
+	*,
+	*::before,
+	*::after {
+		box-sizing: border-box;
+	}
+	
+	:root{
+		--bgcolor: #141938;
+		--blue: #355c7d;
+		--textcolor: #fff;
+		--gradient1: #e78800;
+		--gradient2: #e5ef1a;
+		--green: #00E087;
+		--red: #DE3232;
+		--darkgray: #2f4f4f;
+	}
+
+	html {
+		font-size: 16px;
+	}
+
+	body {
+		margin: 0 20px;
+		padding: 0;
+		background-color: var(--bgcolor);
+		color: var(--textcolor);
+		font-size: 1rem;
+		text-align: center;
+	}
+	
+	
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");const parameters={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},decorators=[Story=>(0,jsx_runtime.jsxs)(jsx_runtime.Fragment,{children:[(0,jsx_runtime.jsx)(GlobalStyle,{}),(0,jsx_runtime.jsx)(Story,{})]})],__namedExportsOrder=["parameters","decorators"];function ownKeys(object,enumerableOnly){var keys=Object.keys(object);if(Object.getOwnPropertySymbols){var symbols=Object.getOwnPropertySymbols(object);enumerableOnly&&(symbols=symbols.filter((function(sym){return Object.getOwnPropertyDescriptor(object,sym).enumerable}))),keys.push.apply(keys,symbols)}return keys}function _defineProperty(obj,key,value){return key in obj?Object.defineProperty(obj,key,{value,enumerable:!0,configurable:!0,writable:!0}):obj[key]=value,obj}Object.keys(preview_namespaceObject).forEach((function(key){var value=preview_namespaceObject[key];switch(key){case"args":return(0,ClientApi.uc)(value);case"argTypes":return(0,ClientApi.v9)(value);case"decorators":return value.forEach((function(decorator){return(0,ClientApi.$9)(decorator,!1)}));case"loaders":return value.forEach((function(loader){return(0,ClientApi.HZ)(loader,!1)}));case"parameters":return(0,ClientApi.h1)(function _objectSpread(target){for(var i=1;i<arguments.length;i++){var source=null!=arguments[i]?arguments[i]:{};i%2?ownKeys(Object(source),!0).forEach((function(key){_defineProperty(target,key,source[key])})):Object.getOwnPropertyDescriptors?Object.defineProperties(target,Object.getOwnPropertyDescriptors(source)):ownKeys(Object(source)).forEach((function(key){Object.defineProperty(target,key,Object.getOwnPropertyDescriptor(source,key))}))}return target}({},value),!1);case"argTypesEnhancers":return value.forEach((function(enhancer){return(0,ClientApi.My)(enhancer)}));case"argsEnhancers":return value.forEach((function(enhancer){return(0,ClientApi._C)(enhancer)}));case"render":return(0,ClientApi.$P)(value);case"globals":case"globalTypes":var v={};return v[key]=value,(0,ClientApi.h1)(v,!1);case"__namedExportsOrder":case"decorateStory":case"renderToDOM":return null;default:return console.log(key+" was not supported :( !")}}))},"./src sync recursive ^\\.(?:(?:^%7C\\/%7C(?:(?:(?%21(?:^%7C\\/)\\.).)*?)\\/)(?%21\\.)(?=.)[^/]*?\\.stories\\.(js%7Cjsx%7Cts%7Ctsx))$":(module,__unused_webpack_exports,__webpack_require__)=>{var map={"./components/AddNewAccount/AddNewAccount.stories.js":"./src/components/AddNewAccount/AddNewAccount.stories.js","./components/BankAccount/BankAccount.stories.js":"./src/components/BankAccount/BankAccount.stories.js","./components/Button/Button.stories.js":"./src/components/Button/Button.stories.js","./components/InputLabel/InputLabel.stories.js":"./src/components/InputLabel/InputLabel.stories.js"};function webpackContext(req){var id=webpackContextResolve(req);return __webpack_require__(id)}function webpackContextResolve(req){if(!__webpack_require__.o(map,req)){var e=new Error("Cannot find module '"+req+"'");throw e.code="MODULE_NOT_FOUND",e}return map[req]}webpackContext.keys=function webpackContextKeys(){return Object.keys(map)},webpackContext.resolve=webpackContextResolve,module.exports=webpackContext,webpackContext.id="./src sync recursive ^\\.(?:(?:^%7C\\/%7C(?:(?:(?%21(?:^%7C\\/)\\.).)*?)\\/)(?%21\\.)(?=.)[^/]*?\\.stories\\.(js%7Cjsx%7Cts%7Ctsx))$"},"./src sync recursive ^\\.(?:(?:^%7C\\/%7C(?:(?:(?%21(?:^%7C\\/)\\.).)*?)\\/)(?%21\\.)(?=.)[^/]*?\\.stories\\.mdx)$":module=>{function webpackEmptyContext(req){var e=new Error("Cannot find module '"+req+"'");throw e.code="MODULE_NOT_FOUND",e}webpackEmptyContext.keys=()=>[],webpackEmptyContext.resolve=webpackEmptyContext,webpackEmptyContext.id="./src sync recursive ^\\.(?:(?:^%7C\\/%7C(?:(?:(?%21(?:^%7C\\/)\\.).)*?)\\/)(?%21\\.)(?=.)[^/]*?\\.stories\\.mdx)$",module.exports=webpackEmptyContext},"./storybook-init-framework-entry.js":(__unused_webpack_module,__unused_webpack___webpack_exports__,__webpack_require__)=>{"use strict";__webpack_require__("./node_modules/@storybook/react/dist/esm/client/index.js")},"?4f7e":()=>{},"./generated-stories-entry.cjs":(module,__unused_webpack_exports,__webpack_require__)=>{"use strict";module=__webpack_require__.nmd(module),(0,__webpack_require__("./node_modules/@storybook/react/dist/esm/client/index.js").configure)([__webpack_require__("./src sync recursive ^\\.(?:(?:^%7C\\/%7C(?:(?:(?%21(?:^%7C\\/)\\.).)*?)\\/)(?%21\\.)(?=.)[^/]*?\\.stories\\.mdx)$"),__webpack_require__("./src sync recursive ^\\.(?:(?:^%7C\\/%7C(?:(?:(?%21(?:^%7C\\/)\\.).)*?)\\/)(?%21\\.)(?=.)[^/]*?\\.stories\\.(js%7Cjsx%7Cts%7Ctsx))$")],module,!1)}},__webpack_require__=>{var __webpack_exec__=moduleId=>__webpack_require__(__webpack_require__.s=moduleId);__webpack_require__.O(0,[667],(()=>(__webpack_exec__("./node_modules/@storybook/core-client/dist/esm/globals/polyfills.js"),__webpack_exec__("./node_modules/@storybook/core-client/dist/esm/globals/globals.js"),__webpack_exec__("./storybook-init-framework-entry.js"),__webpack_exec__("./node_modules/@storybook/react/dist/esm/client/docs/config-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/react/dist/esm/client/preview/config-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-links/preview.js-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-docs/preview.js-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-actions/preview.js-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-backgrounds/preview.js-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-measure/preview.js-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-outline/preview.js-generated-config-entry.js"),__webpack_exec__("./node_modules/@storybook/addon-interactions/preview.js-generated-config-entry.js"),__webpack_exec__("./.storybook/preview.js-generated-config-entry.js"),__webpack_exec__("./generated-stories-entry.cjs"))));__webpack_require__.O()}]);
+//# sourceMappingURL=main.838f06af.iframe.bundle.js.map
