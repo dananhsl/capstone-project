a4c68b177d7a2f1e6256ff28fdf4d011319d6527
Philipp Enz <programming@phenz.de>
2022-06-30 16:03:10 +0200

    remove comments

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -31,39 +31,10 @@ const useStore = create(set => ({
 			}
 			return bankAccount;
 		});
-		set(state => {
+		set(() => {
 			return {db: updateDb};
 		});
 	},
 }));
 
 export default useStore;
-
-//Second attempt
-// const updateDb = state.db.map(bankAccount => {
-// 	if (bankAccount.id === id) {
-// 		return (bankAccount.entries = [
-// 			...bankAccount.entries,
-// 			{id: nanoid(), ...transaction},
-// 		]);
-// 	} else {
-// 		return bankAccount;
-// 	}
-// });
-// return {db: [...updateDb]};
-
-/*
-	First Attempt 
-	Bug detected: 
-	add new transaction works but added it aswell to the bankaccounts
-*/
-// const filteredAccount = db.find(bankAccount => bankAccount.id === id);
-// 			return {
-// 				db: [
-// 					...state.db,
-
-// 					(filteredAccount.entries = [
-// 						...filteredAccount.entries,
-// 						{id: nanoid(), ...transaction},
-// 					]),
-// 				],
