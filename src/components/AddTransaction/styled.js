875aeffb4cbf38f256be77b7dddd9f3626bc2f99
Dana Neuhäusel <skadi@Danas-MacBook-Air.local>
2022-07-08 17:10:44 +0200

    change styling

--- src/components/AddTransaction/styled.js
+++ src/components/AddTransaction/styled.js
@@ -5,28 +5,9 @@ export const BackButton = styled.button`
 	position: relative;
 	align-items: center;
 	margin: 2em;
-	padding: 2em 1.5em;
-	border: none;
+	padding: 1em;
+	border: 1.5px solid white;
+	border-radius: 15px;
 	background: none;
 	color: white;
-
-	&::before {
-		content: '';
-		position: absolute;
-		z-index: -1;
-		top: 50%;
-		width: 45px;
-		height: 45px;
-		transform: translateY(-50%) translateX(calc(100% + 4px));
-		transition: transform 0.25s 0.25s cubic-bezier(0, 0, 0.5, 2);
-		border-radius: 50px;
-		background-color: var(--gradient1);
-	}
-
-	&:hover::before {
-		width: 100%;
-		transform: translateY(-50%) translateX(-18px);
-		transition: transform 0.25s cubic-bezier(0, 0, 0.5, 2),
-			width 0.25s 0.25s cubic-bezier(0, 0, 0.5, 2);
-	}
 `;
