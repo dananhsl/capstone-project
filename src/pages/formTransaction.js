47f2fba3ad1bddf263e6a749bb8e5d1703d65464
Dana Neuhäusel <skadi@Danas-MacBook-Air.local>
2022-07-08 11:50:28 +0200

    add navigation back to transaction history after editing transaction

--- src/pages/formTransaction.js
+++ src/pages/formTransaction.js
@@ -3,7 +3,7 @@ import AddTransaction from '../components/AddTransaction';
 export default function FormTransaction() {
 	return (
 		<>
-			<h1>Add a new Transaction</h1>
+			<h1>Transaction</h1>
 			<AddTransaction />
 		</>
 	);
