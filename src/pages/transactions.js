57a9dfb21620f05efea90330dade4802085c35a1
Dana Neuhäusel <skadi@Danas-MacBook-Air.local>
2022-07-10 20:10:48 +0200

    show balance on transaction history page

--- src/pages/transactions.js
+++ src/pages/transactions.js
@@ -1,4 +1,5 @@
 import Button from '../components/Button';
+import AccountBalance from '../components/Transaction/AccountBalance';
 import Transactions from '../components/Transaction/Transactions';
 
 export default function TransactionHistory() {
@@ -6,6 +7,7 @@ export default function TransactionHistory() {
 		<>
 			<h1>Your recent transactions:</h1>
 			<Button />
+			<AccountBalance />
 			<Transactions />
 		</>
 	);
