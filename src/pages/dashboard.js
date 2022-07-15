2f511ae2a5508a4484f2c4344869dfae440bd113
Philipp Enz <programming@phenz.de>
2022-07-15 10:25:01 +0200

    add react charts and test if working properly

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,12 @@
+import Pie from '../components/Analysis';
 import BankAccounts from '../components/BankAccount/BankAccounts.js';
-
 
 export default function Dashboard() {
 	return (
 		<>
 			<h1>Dashboard</h1>
+			<Pie />
+			<br></br>
 			<BankAccounts />
 		</>
 	);
