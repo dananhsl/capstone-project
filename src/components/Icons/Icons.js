1c4f6af35682daa4d921a0bd7112276162252452
Philipp Enz <programming@phenz.de>
2022-07-16 18:23:47 +0200

    add svgs, aria-labels and styling to catbuttons

---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,13 @@
+import {StyledIcon} from './styled';
+
+export const editIcon = (
+	<StyledIcon viewBox="0 0 24 24">
+		<path d="M16.84,2.73C16.45,2.73 16.07,2.88 15.77,3.17L13.65,5.29L18.95,10.6L21.07,8.5C21.67,7.89 21.67,6.94 21.07,6.36L17.9,3.17C17.6,2.88 17.22,2.73 16.84,2.73M12.94,6L4.84,14.11L7.4,14.39L7.58,16.68L9.86,16.85L10.15,19.41L18.25,11.3M4.25,15.04L2.5,21.73L9.2,19.94L8.96,17.78L6.65,17.61L6.47,15.29" />
+	</StyledIcon>
+);
+
+export const deleteIcon = (
+	<StyledIcon viewBox="0 0 24 24">
+		<path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+	</StyledIcon>
+);
