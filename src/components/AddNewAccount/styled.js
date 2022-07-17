d0c3265da5bdc44142e9363ed219e860421722a6
Philipp Enz <programming@phenz.de>
2022-07-17 10:47:43 +0200

    add styling to account from

--- src/components/AddNewAccount/styled.js
+++ src/components/AddNewAccount/styled.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 export const Form = styled.form`
 	display: flex;
 	flex-direction: column;
+	padding: 1em;
+	border: 2px solid var(--textcolor);
+	border-radius: 25px;
+	background: linear-gradient(to bottom, var(--blue), var(--bgcolor));
+	box-shadow: rgb(0 0 0 / 80%) 4px 4px 15px;
 `;
