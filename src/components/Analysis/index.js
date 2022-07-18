b3922dd528e36201ec2458b2d08aeba037487c08
Philipp Enz <programming@phenz.de>
2022-07-18 12:44:14 +0200

    add styling to analysis

--- src/components/Analysis/index.js
+++ src/components/Analysis/index.js
@@ -20,8 +20,10 @@ export default function Analysis() {
 	const options = {
 		plugins: {
 			legend: {
+				position: 'bottom',
 				labels: {
 					color: 'white',
+					font: {size: '15em'},
 				},
 			},
 		},
@@ -37,14 +39,14 @@ export default function Analysis() {
 				label: 'Value ',
 				data: [...displayData],
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)',
-					'rgba(208,64,224,0.2)',
-					'rgba(64,224,208,0.2)',
+					'rgba(255, 99, 132, 0.5)',
+					'rgba(54, 162, 235, 0.5)',
+					'rgba(255, 206, 86, 0.5)',
+					'rgba(75, 192, 192, 0.5)',
+					'rgba(153, 102, 255, 0.5)',
+					'rgba(255, 159, 64, 0.5)',
+					'rgba(208,64,224,0.5)',
+					'rgba(64,224,208,0.5)',
 				],
 				borderColor: [
 					'rgba(255, 99, 132, 1)',
@@ -53,8 +55,8 @@ export default function Analysis() {
 					'rgba(75, 192, 192, 1)',
 					'rgba(153, 102, 255, 1)',
 					'rgba(255, 159, 64, 1)',
-					'rgba(208,64,224,0.2)',
-					'rgba(64,224,208,0.2)',
+					'rgba(208,64,224,1)',
+					'rgba(64,224,208,1)',
 				],
 				borderWidth: 1,
 			},
--- src/components/Analysis/styled.js
+++ src/components/Analysis/styled.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledPieContainer = styled.div`
+	height: 60vh;
 	margin-top: 5em;
 	margin-right: 1em;
-	padding: 0.5em;
-	border: 2px solid var(--blue);
+	padding: 2em 0;
+	border: 1px solid white;
 	border-radius: 25px;
+	background: linear-gradient(to bottom, var(--blue), var(--bgcolor));
+	box-shadow: rgb(0 0 0 / 80%) 4px 4px 15px;
 `;
